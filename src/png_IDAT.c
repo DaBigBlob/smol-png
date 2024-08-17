74641f5d76a69c11e7a066429e8071846ae82a7b
Person <localboxcrox@gmail.com>
2024-08-17 14:05:41 -0400

    removr bin

--- src/png_IDAT.c
+++ src/png_IDAT.c
@@ -17,29 +17,22 @@ typedef bytes png_IDAT_data;
 
 png_IDAT_data* serialize_png_IDAT(png_IDAT_struct* istr) {
     bytes* pid = new_bytes(255);
-    if (!pid) return NULL;
+    if (!pid) goto ret;
 
-    if (add_uint32(pid, istr->len) != 0) {
-        free_bytes(pid);
-        return NULL;
-    }
+    if (add_uint32(pid, istr->len) != 0) goto bad;
 
-    if(add_bytes_from(pid, IDAT_type_sig, sizeof(IDAT_type_sig), false) != 0) {
-        free_bytes(pid);
-        return NULL;
-    }
+    if(add_bytes_from(pid, IDAT_type_sig, sizeof(IDAT_type_sig), false) != 0) goto bad;
 
-    if(add_bytes(pid, istr->zlibstream, istr->zlibstream->len, true) != 0) {
-        free_bytes(pid);
-        return NULL;
-    }
+    if(add_bytes(pid, istr->zlibstream, istr->zlibstream->len, true) != 0) goto bad;
 
-    if (add_uint32(pid, istr->crc) != 0) {
-        free_bytes(pid);
-        return NULL;
-    }
+    if (add_uint32(pid, istr->crc) != 0) goto bad;
 
     return pid;
+
+    bad:
+        free_bytes(pid);
+    ret:
+        return NULL;
 }
 
 uint8_t populate_crc(png_IDAT_struct* istr) {
